6f9aedd1a0328e439a8339969409fd6ff682b9bc
Jason Swett <jason@benfranklinlabs.com>
2019-10-01 16:53:36 -0400

    Update SHA.

--- Formula/exosuit.rb
+++ Formula/exosuit.rb
@@ -2,7 +2,7 @@ class Exosuit < Formula
   desc 'Exosuit'
   homepage 'https://github.com/jasonswett/exosuit'
   url 'https://exosuit.s3.us-east-2.amazonaws.com/exosuit-0.0.2.tar.gz'
-  sha256 'abce0a624167434d12e105c3cdaf40cebf4d9a5799830abb8c7aa8aef0920673'
+  sha256 'e5f4a83e709bae3643fd3e99839149683a11b2864ed2431d1447d9204e773483'
 
   def install
 		libexec.install Dir["*"]

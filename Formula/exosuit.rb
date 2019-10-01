31f6bb930142ef0339ec9cdd1a851add4291368a
Jason Swett <jason@benfranklinlabs.com>
2019-10-01 16:52:41 -0400

    Update version to 0.0.2.

--- Formula/exosuit.rb
+++ Formula/exosuit.rb
@@ -1,7 +1,7 @@
 class Exosuit < Formula
   desc 'Exosuit'
   homepage 'https://github.com/jasonswett/exosuit'
-  url 'https://exosuit.s3.us-east-2.amazonaws.com/exosuit-0.0.1.tar.gz'
+  url 'https://exosuit.s3.us-east-2.amazonaws.com/exosuit-0.0.2.tar.gz'
   sha256 'abce0a624167434d12e105c3cdaf40cebf4d9a5799830abb8c7aa8aef0920673'
 
   def install

c1336e8a812292f202631af24397938f9542818b
Jason Swett <jason@benfranklinlabs.com>
2019-10-08 16:15:59 -0400

    Bump version to 0.0.5.

--- Formula/exosuit.rb
+++ Formula/exosuit.rb
@@ -1,8 +1,8 @@
 class Exosuit < Formula
   desc 'Exosuit'
   homepage 'https://github.com/jasonswett/exosuit'
-  url 'https://exosuit.s3.us-east-2.amazonaws.com/exosuit-0.0.4.tar.gz'
-  sha256 '740b3d0d58ef25647c90ebf0766dc8a010463108525260f91ab20afd452d5719'
+  url 'https://exosuit.s3.us-east-2.amazonaws.com/exosuit-0.0.5.tar.gz'
+  sha256 'f4be2182d9ec99b7f52c8e9706f20d9bdc206b708c94589bf4e8090e9bb8c0f5'
 
   def install
 		libexec.install Dir["*"]
